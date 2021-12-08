d40621a0fada4a9ca6ed4095e2305b1873d34787
Charles Kanoy <ch.kanoy@gmail.com>
2021-12-07 21:00:45 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🪐Conway's Game of Life🪐
+
+This project is to get more familiar with C/C++.
+
+## Goals
+
+- [ ] CGOL algorithm
+- [ ] GUI that allows you to customize that allows bounds customization
---
+++ cgol.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* declare array with population
+apply rules:
+    1. any live cell 2 or 3 live neighbors lives
+    2. any live cell with 1 neighbor dies
+    3. any dead cell with 3 neighbors lives
+    4. any live cell with more than 3 live neighbors dies
+repeat */
+
+void main() {
+    printf("hello");
+}
